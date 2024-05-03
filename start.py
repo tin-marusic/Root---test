35525f396246f618ded7a0e886e29ee32e7ae4c3
Tin Marusic <tmarusic@lxplus811.cern.ch>
2024-05-04 01:34:09 +0200

    Ispravak preklapanja

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 import uproot
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 root = uproot.open("ntuple_doublePhotons_1k_OKAY.root")
 tree = root["l1tHGCalTriggerNtuplizer/HGCalTriggerNtuple;20"]
@@ -21,7 +22,44 @@ def eta_index(array): #Funkcija koja trazi na kojem je indeksu gen_eta>0, u gen_
     else:
         return 1
 
-event = 5644
+def obrisi_datoteke_iz_mape(putanja_do_mape):
+    for datoteka in os.listdir(putanja_do_mape):
+        putanja_do_datoteke = os.path.join(putanja_do_mape, datoteka)
+        os.remove(putanja_do_datoteke)
+        
+def dodaj_u_liste(list_x,list_y,list_e,x,y,e): #funkcija koja ce sluziti za sumiranje eneragija svih layera
+    preklapanje = False
+    for i in range(len(list_x)):
+        if abs(list_x[i] - x) <= 10.5 and abs(list_y[i] - y) <= 10.5: #provjera imamo li preklapanje 
+            list_e[i] += e
+            preklapanje = True
+            break
+    if not preklapanje: #ako nismo imali preklapanje samo dodajemo tu koordinatu u liste
+        list_x.append(x)
+        list_y.append(y)
+        list_e.append(e)
+                    
+    return list_x,list_y,list_e
+
+def plot_hexagon(list_x,list_y,list_e,i):
+    #izrada heksagonskog plota
+    if len(list_e) > 1:
+        ht = plt.hexbin(list_y,list_x, C=list_e, cmap='plasma', gridsize=23, vmax=(np.max(list_e)), vmin=np.min(list_e),extent=[-125,125,-125,125])
+    else:
+        ht = plt.hexbin(list_y,list_x, C=list_e, cmap='plasma', gridsize=23, vmax=(np.max(list_e)), vmin=np.min(list_e) - 0.05,extent=[-125,125,-125,125])
+    #print(f"{i}. : {list_x},{list_y}")
+    cb = plt.colorbar(ht)  
+    cb.set_label("vrijednost energije")
+    plt.xlabel("ts_y")
+    plt.ylabel("ts_x")
+    plt.title(f"ts_y:ts_x:ts_energy   |   ts_layer == {i}, event == {event} \n gen_energy = {gen_energy:.4f}    |    gen_pt = {gen_pt:.4f}")
+    plt.savefig(f"Plotovi/plot{i}.png")
+    plt.clf()
+        
+obrisi_datoteke_iz_mape("Plotovi")
+obrisi_datoteke_iz_mape("Preklapanja")
+
+event = 4674
 
 event_indeks = get_index(branches["event"],event)[0] #dobivamo indeks layera, on je jedinstven u banchu layers
 ts_layer = np.asarray(branches["ts_layer"][event_indeks]) #prebacujemo se u numpy oblik podataka
@@ -31,9 +69,13 @@ gen_eta_index = eta_index(branches["gen_eta"][event_indeks])
 gen_energy = branches["gen_energy"][event_indeks][gen_eta_index]
 gen_pt = branches["gen_pt"][event_indeks][gen_eta_index]
 
-# Izračuna minimum i maksimum
+# Izračuni minimum i maksimum
 min_layer = np.min(ts_layer)
 max_layer = np.max(ts_layer)
+
+suma_list_x = []
+suma_list_y = []
+suma_list_e = []
 
 # Iteracija kroz layere
 for i in range(min_layer, max_layer+1):
@@ -45,14 +87,36 @@ for i in range(min_layer, max_layer+1):
         for j in layer_indeks: #stvaramo liste koordinata i energija za odredeni layer
             list_x = np.append(list_x,[branches["ts_x"][event_indeks][j]]) 
             list_y = np.append(list_y,[branches["ts_y"][event_indeks][j]]) 
-            list_e = np.append(list_e,[branches["ts_energy"][event_indeks][j]])  
+            list_e = np.append(list_e,[branches["ts_energy"][event_indeks][j]])
+            suma_list_x,suma_list_y,suma_list_e = dodaj_u_liste(suma_list_x,suma_list_y,suma_list_e,branches["ts_x"][event_indeks][j],
+                                                                branches["ts_y"][event_indeks][j],branches["ts_energy"][event_indeks][j])  
         
-        ht = plt.hexbin(list_x,list_y, C=list_e, cmap='plasma', gridsize=35, vmax=(np.max(list_e)), vmin=np.min(list_e),extent=[-125,125,-125,125])
-        #print(f"{i}. : {list_x},{list_y}")
-        cb = plt.colorbar(ht)  
-        cb.set_label("vrijednost energije")
-        plt.xlabel("ts_x")
-        plt.ylabel("ts_y")
-        plt.title(f"ts_x:ts_y:ts_energy   |   ts_layer == {i}, event == {event} \n gen_energy = {gen_energy:.4f}    |    gen_pt = {gen_pt:.4f}")
-        plt.savefig(f"Plotovi/plot{i}.png")
-        plt.clf()
+        f = open(f"Preklapanja/preklapanja_layer_{i}.txt","w")
+        #provjera preklapanja koordinata tocaka
+        for j in range(len(list_x)):
+            for k in range(j + 1, len(list_x)):
+                try:
+                    if abs(list_x[k] - list_x[j]) <= 10.5 and abs(list_y[k] - list_y[j]) <= 10.5: #10.5 je priblizna apotema heksagona za parametre plota koje koristimo
+                        f.write(f"preklapanje na koordinatama ({list_x[j],list_y[j]}),({list_x[k],list_y[k]}) - zbrojene energije {list_e[j]} i {list_e[k]} \n")
+                        #zbrajamo energije na mjestu gdje je nastalo preklapanje
+                        list_e[j] += list_e[k]
+                        # Jednu koordinatu preklapanja brisemo, to su ove tocke. Ne mozemo ih brisati u petlji pa ih postavljamo na vrijednost koja nece utjecati na petlju
+                        list_x[k] = np.nan
+                        list_y[k] = np.nan
+                        list_e[k] = np.nan
+                except: #slucaj kada se oduzima broj i np.nan
+                    continue
+        
+        #Uklanjamo preklopljene vrijednosti, tj uklanjamo np.nan elemente iz liste 
+        list_x = list_x[~np.isnan(list_x)]
+        list_y = list_y[~np.isnan(list_y)]
+        list_e = list_e[~np.isnan(list_e)]
+        f.close()
+        
+        #brisemo prazne datoteke - layeri u kojima nije bilo preklapanja
+        if os.path.getsize(f"Preklapanja/preklapanja_layer_{i}.txt") == 0:
+            os.remove(f"Preklapanja/preklapanja_layer_{i}.txt")
+        
+        plot_hexagon(list_x,list_y,list_e,i)
+        
+plot_hexagon(suma_list_x,suma_list_y,suma_list_e,"suma svih")        
